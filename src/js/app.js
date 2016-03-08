ae268abd6a5cf5a85127c8e03b1cbd80d7846095
Wen <Wen@wen.local>
2016-03-08 15:30:49 +0000

    add page builds

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+// app.js
+(function() {
+	var App = function() {
+		if(document.body) this._init();
+		else {
+			window.addEventListener("load", this._init.bind(this));
+		}
+	}
+
+	var p = App.prototype;
+
+	p._init = function() {
+	};
+
+	new App();
+})();
+
+
