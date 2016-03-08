afd37164bbfd1b9605e9a728167d7e2a038f6fbe
Wen <Wen@wen.local>
2016-03-08 18:15:24 +0000

    add dummy data

--- dist/bundle/bundle.js
+++ dist/bundle/bundle.js
@@ -15,6 +15,33 @@ var Model = [{
 }, {
 	cover: 'assets/sketchCoverFlocking.jpg',
 	url: 'urlPath2'
+}, {
+	cover: 'assets/sketchCoverParticles.jpg',
+	url: 'urlPath0'
+}, {
+	cover: 'assets/sketchCoverReflection.jpg',
+	url: 'urlPath1'
+}, {
+	cover: 'assets/sketchCoverFlocking.jpg',
+	url: 'urlPath2'
+}, {
+	cover: 'assets/sketchCoverParticles.jpg',
+	url: 'urlPath0'
+}, {
+	cover: 'assets/sketchCoverReflection.jpg',
+	url: 'urlPath1'
+}, {
+	cover: 'assets/sketchCoverFlocking.jpg',
+	url: 'urlPath2'
+}, {
+	cover: 'assets/sketchCoverParticles.jpg',
+	url: 'urlPath0'
+}, {
+	cover: 'assets/sketchCoverReflection.jpg',
+	url: 'urlPath1'
+}, {
+	cover: 'assets/sketchCoverFlocking.jpg',
+	url: 'urlPath2'
 }];
 
 exports.default = Model;
--- src/js/Model.js
+++ src/js/Model.js
@@ -12,6 +12,42 @@ const Model = [
 	{
 		cover:'assets/sketchCoverFlocking.jpg',
 		url:'urlPath2'
+	},
+	{
+		cover:'assets/sketchCoverParticles.jpg',
+		url:'urlPath0'
+	},
+	{
+		cover:'assets/sketchCoverReflection.jpg',
+		url:'urlPath1'
+	},
+	{
+		cover:'assets/sketchCoverFlocking.jpg',
+		url:'urlPath2'
+	},
+	{
+		cover:'assets/sketchCoverParticles.jpg',
+		url:'urlPath0'
+	},
+	{
+		cover:'assets/sketchCoverReflection.jpg',
+		url:'urlPath1'
+	},
+	{
+		cover:'assets/sketchCoverFlocking.jpg',
+		url:'urlPath2'
+	},
+	{
+		cover:'assets/sketchCoverParticles.jpg',
+		url:'urlPath0'
+	},
+	{
+		cover:'assets/sketchCoverReflection.jpg',
+		url:'urlPath1'
+	},
+	{
+		cover:'assets/sketchCoverFlocking.jpg',
+		url:'urlPath2'
 	}
 ]
 
