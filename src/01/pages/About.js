b93e39bd619bcc41ac01ea388e3809494808f5c2
Wen <Wen@wen.local>
2016-03-11 15:00:19 +0000

    about page

--- src/01/pages/About.js
+++ src/01/pages/About.js
@@ -7,12 +7,22 @@ import { push } from 'react-router-redux'
 
 const About = (props) => {
 	return (
-		<div
-			className='About-Container' 
-			onClick={ ()=>{
-				props.dispatch(push('/'));
-			}
-		}>About container</div>
+		<div className='About-Container' >
+			<div className="About-RightPanel">
+				<div className="About-TextWrapper">	
+					<p className="About-Desc">A place for my sketches, most of them are not optimised so it take a bit of time to load, please be patient. Also some of them aren't working in Firefox because Firefox doesn't support textureCubeLod</p>
+					<a href="http://blog.bongiovi.tw" target="_blank"><p className="About-Link">blog.bongiovi.tw</p></a>
+					<a href="https://twitter.com/yiwen_lin" target="_blank"><p className="About-Link">@yiwen_lin</p></a>
+				</div>
+			</div>
+			<div 
+				className="About-Background"
+				onClick={ ()=>{
+					console.log('Click');
+					props.dispatch(push('/'));
+				}}
+			/>
+		</div>
 	);
 }
 
