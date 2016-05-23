36f0a8ad094d83de5e9924a52a0890aac2a97a5a
yiwenl <bongiovi015@gmail.com>
2016-05-23 23:07:37 +0100

    update front page using webpack
    Signed-off-by: yiwenl <bongiovi015@gmail.com>

---
+++ src/js/ExpModel.js
@@ -0,0 +1,72 @@
+const ExpModel = [
+	{
+		cover:'assets/img/sketchCoverParticles.jpg',
+		url:'http://yiwenl.github.io/Sketches/experiments/selfshadingParticles/dist/index.html',
+		opened:false
+	},
+	{
+		cover:'assets/img/sketchCoverReflection.jpg',
+		url:'http://yiwenl.github.io/Sketches/experiments/reflectiveSoundVis/dist/index.html',
+		opened:false
+	},
+	{
+		cover:'assets/img/sketchCoverNefertiti.jpg',
+		url:'http://yiwenl.github.io/Sketches/experiments/Nefertiti/dist/index.html',
+		opened:false
+	},
+	{
+		cover:'assets/img/sketchCoverFlocking.jpg',
+		url:'http://yiwenl.github.io/Sketches/experiments/flockingBW/dist/index.html',
+		opened:false
+	},
+	{
+		cover:'assets/img/sketchCoverRosetta.jpg',
+		url:'http://yiwenl.github.io/Sketches/experiments/rosetta/dist/index.html',
+		opened:false
+	},
+	{
+		cover:'assets/img/coverSketchNike.jpg',
+		url:'http://yiwenl.github.io/Sketches/experiments/Bloom/dist/index.html',
+		opened:false
+	},
+	{
+		cover:'assets/img/coverSketchBloom.jpg',
+		url:'http://yiwenl.github.io/Sketches/experiments/feathers/dist/index.html',
+		opened:false
+	},
+	{
+		cover:'assets/img/coverSketchClustering.jpg',
+		url:'http://yiwenl.github.io/Sketches/experiments/flocking01/dist/index.html',
+		opened:false
+	},
+	{
+		cover:'assets/img/coverSketchRefraction.jpg',
+		url:'http://yiwenl.github.io/Sketches/experiments/Refaction/dist/index.html',
+		opened:false
+	},
+	{
+		cover:'assets/img/textureProj.jpg',
+		url:'http://yiwenl.github.io/Sketches/experiments/mask/dist/index.html',
+		opened:false
+	},
+	{
+		cover:'assets/img/costume.jpg',
+		url:'http://yiwenl.github.io/Sketches/experiments/wrap/dist/index.html',
+		opened:false
+	},
+	{
+		cover:'assets/img/seasons.jpg',
+		url:'http://yiwenl.github.io/Sketches/experiments/blossom/dist/index.html',
+		opened:false
+	},
+	{
+		cover:'assets/img/river.jpg',
+		url:'http://yiwenl.github.io/Sketches/experiments/River/dist/index.html',
+		opened:false
+	}
+]
+
+
+
+
+export default ExpModel;
