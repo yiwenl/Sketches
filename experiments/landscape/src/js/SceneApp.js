cd3398c0bbdcf15caabbff6854d507e56022e8b5
wen <bongiovi015@gmail.com>
2016-06-13 22:20:01 +0100

    add landscape exp

---
+++ experiments/landscape/src/js/SceneApp.js
@@ -0,0 +1,45 @@
+// SceneApp.js
+
+import alfrid, { Scene } from 'alfrid';
+
+const GL = alfrid.GL;
+
+class SceneApp extends alfrid.Scene {
+	constructor() {
+		super();
+		GL.enableAlphaBlending();
+		this.orbitalControl.rx.value = this.orbitalControl.ry.value = 0.3;
+	}
+
+	_initTextures() {
+		console.log('init textures');
+	}
+
+
+	_initViews() {
+		console.log('init views');
+
+		this._bCopy = new alfrid.BatchCopy();
+		this._bAxis = new alfrid.BatchAxis();
+		this._bDots = new alfrid.BatchDotsPlane();
+		this._bBall = new alfrid.BatchBall();
+	}
+
+
+	render() {
+		this.orbitalControl.ry.value += 0.01;
+		GL.clear(0, 0, 0, 0);
+		this._bAxis.draw();
+		this._bDots.draw();
+
+	}
+
+
+	resize() {
+		GL.setSize(window.innerWidth, window.innerHeight);
+		this.camera.setAspectRatio(GL.aspectRatio);
+	}
+}
+
+
+export default SceneApp;
