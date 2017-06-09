a578e105d3c7378de00609e02bfc1d1a0332db90
Yi-Wen Lin <bongiovi015@gmail.com>
2017-06-09 17:55:58 +0100

    maelstrom

---
+++ experiments/maelstrom/src/js/SceneApp.js
@@ -0,0 +1,117 @@
+// SceneApp.js
+
+import alfrid, { Scene, GL } from 'alfrid';
+import ViewSave from './ViewSave';
+import ViewRender from './ViewRender';
+import ViewSim from './ViewSim';
+
+window.getAsset = function(id) {
+	return assets.find( (a) => a.id === id).file;
+}
+
+class SceneApp extends alfrid.Scene {
+	constructor() {
+		super();
+		GL.enableAlphaBlending();
+
+		this._count = 0;
+		this.camera.setPerspective(Math.PI/2, GL.aspectRatio, .1, 100);
+		this.orbitalControl.radius.value = 10;
+		this.orbitalControl.rx.value = this.orbitalControl.ry.value = 0.3;
+	}
+
+	_initTextures() {
+		console.log('init textures');
+
+		//	FBOS
+		const numParticles = params.numParticles;
+		const o = {
+			minFilter:GL.NEAREST,
+			magFilter:GL.NEAREST
+		};
+
+		this._fboCurrent  	= new alfrid.FrameBuffer(numParticles, numParticles, o, true);
+		this._fboTarget  	= new alfrid.FrameBuffer(numParticles, numParticles, o, true);
+	}
+
+
+	_initViews() {
+		console.log('init views');
+		
+		//	helpers
+		this._bCopy = new alfrid.BatchCopy();
+		this._bAxis = new alfrid.BatchAxis();
+		this._bDots = new alfrid.BatchDotsPlane();
+		this._bBall = new alfrid.BatchBall();
+
+
+		//	views
+		this._vRender = new ViewRender();
+		this._vSim 	  = new ViewSim();
+
+		this._vSave = new ViewSave();
+		GL.setMatrices(this.cameraOrtho);
+
+
+		this._fboCurrent.bind();
+		GL.clear(0, 0, 0, 0);
+		this._vSave.render();
+		this._fboCurrent.unbind();
+
+		this._fboTarget.bind();
+		GL.clear(0, 0, 0, 0);
+		this._vSave.render();
+		this._fboTarget.unbind();
+
+		GL.setMatrices(this.camera);
+	}
+
+
+	updateFbo() {
+		this._fboTarget.bind();
+		GL.clear(0, 0, 0, 1);
+		this._vSim.render(this._fboCurrent.getTexture(1), this._fboCurrent.getTexture(0), this._fboCurrent.getTexture(2));
+		this._fboTarget.unbind();
+
+
+		let tmp          = this._fboCurrent;
+		this._fboCurrent = this._fboTarget;
+		this._fboTarget  = tmp;
+
+	}
+
+
+	render() {
+
+		this._count ++;
+		if(this._count % params.skipCount == 0) {
+			this._count = 0;
+			this.updateFbo();
+		}
+
+		let p = this._count / params.skipCount;
+
+		GL.clear(0, 0, 0, 0);
+		this._bAxis.draw();
+		this._bDots.draw();
+
+		this._vRender.render(this._fboTarget.getTexture(0), this._fboCurrent.getTexture(0), p, this._fboCurrent.getTexture(2));
+
+		const size = Math.min(params.numParticles, GL.height/4);
+
+		for(let i=0; i<4; i++) {
+			GL.viewport(0, size * i, size, size);
+			this._bCopy.draw(this._fboCurrent.getTexture(i));
+		}
+
+	}
+
+
+	resize() {
+		GL.setSize(window.innerWidth, window.innerHeight);
+		this.camera.setAspectRatio(GL.aspectRatio);
+	}
+}
+
+
+export default SceneApp;
