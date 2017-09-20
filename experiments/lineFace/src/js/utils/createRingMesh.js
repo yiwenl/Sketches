e130dd53993ea75512875a3228e2b5d6b1b15211
Yi-Wen Lin <bongiovi015@gmail.com>
2017-09-20 17:31:03 +0100

    Add sides

--- experiments/lineFace/src/js/utils/createRingMesh.js
+++ experiments/lineFace/src/js/utils/createRingMesh.js
@@ -3,6 +3,7 @@
 import alfrid, { Mesh } from 'alfrid';
 
 const num = 20;
+
 var random = function(min, max) { return min + Math.random() * (max - min);	}
 
 const getPos = (i, y, height, radius, theta) => {
@@ -23,34 +24,151 @@ const createRingMesh = (y, height, radius, theta) => {
 	const uvs = [];
 	const indices = [];
 	const extras = [];
+	const thickness = random(.2, .4);
 	let count = 0;
 
 	const rotation = Math.random() * Math.PI * 2;
 	const speed = random(.2, .6);
 
+	// left
+	positions.push(getPos(0, y, -height, radius-thickness, theta));
+	positions.push(getPos(0, y, -height, radius, theta));
+	positions.push(getPos(0, y,  height, radius, theta));
+	positions.push(getPos(0, y,  height, radius-thickness, theta));
+
+	uvs.push([0, 0]);
+	uvs.push([1, 0]);
+	uvs.push([1, 1]);
+	uvs.push([0, 1]);
+
+	normals.push([0, 0, -1]);
+	normals.push([0, 0, -1]);
+	normals.push([0, 0, -1]);
+	normals.push([0, 0, -1]);
+
+	indices.push(count * 4 + 0);
+	indices.push(count * 4 + 1);
+	indices.push(count * 4 + 2);
+	indices.push(count * 4 + 0);
+	indices.push(count * 4 + 2);
+	indices.push(count * 4 + 3);
+	
+	extras.push([rotation, speed]);
+	extras.push([rotation, speed]);
+	extras.push([rotation, speed]);
+	extras.push([rotation, speed]);
+
+	count ++;
+
+	// right
+
+	positions.push(getPos(num, y, -height, radius, theta));
+	positions.push(getPos(num, y, -height, radius-thickness, theta));
+	positions.push(getPos(num, y,  height, radius-thickness, theta));
+	positions.push(getPos(num, y,  height, radius, theta));
+
+	uvs.push([0, 0]);
+	uvs.push([1, 0]);
+	uvs.push([1, 1]);
+	uvs.push([0, 1]);
+
+	normals.push([0, 0, -1]);
+	normals.push([0, 0, -1]);
+	normals.push([0, 0, -1]);
+	normals.push([0, 0, -1]);
+
+	indices.push(count * 4 + 0);
+	indices.push(count * 4 + 1);
+	indices.push(count * 4 + 2);
+	indices.push(count * 4 + 0);
+	indices.push(count * 4 + 2);
+	indices.push(count * 4 + 3);
+	
+	extras.push([rotation, speed]);
+	extras.push([rotation, speed]);
+	extras.push([rotation, speed]);
+	extras.push([rotation, speed]);
+
+	count ++;
+
 	for(let i=0; i<num; i++) {
+		//	front
 		positions.push(getPos(i, y, -height, radius, theta));
 		positions.push(getPos(i+1, y, -height, radius, theta));
 		positions.push(getPos(i+1, y,  height, radius, theta));
 		positions.push(getPos(i, y,  height, radius, theta));
 
-
-		//*/
 		uvs.push([i/num, 0]);
 		uvs.push([(i+1)/num, 0]);
 		uvs.push([(i+1)/num, 1]);
 		uvs.push([i/num, 1]);
-		/*/
-		uvs.push([0, 0]);
-		uvs.push([1, 0]);
-		uvs.push([1, 1]);
-		uvs.push([0, 1]);
-		//*/
 
 		normals.push([0, 0, -1]);
 		normals.push([0, 0, -1]);
 		normals.push([0, 0, -1]);
 		normals.push([0, 0, -1]);
+
+		indices.push(count * 4 + 0);
+		indices.push(count * 4 + 1);
+		indices.push(count * 4 + 2);
+		indices.push(count * 4 + 0);
+		indices.push(count * 4 + 2);
+		indices.push(count * 4 + 3);
+		
+		extras.push([rotation, speed]);
+		extras.push([rotation, speed]);
+		extras.push([rotation, speed]);
+		extras.push([rotation, speed]);
+
+		count ++;
+
+
+		//	top
+		positions.push(getPos(i, y, -height, radius-thickness, theta));
+		positions.push(getPos(i+1, y, -height, radius-thickness, theta));
+		positions.push(getPos(i+1, y, -height, radius, theta));
+		positions.push(getPos(i, y, -height, radius, theta));
+
+		uvs.push([i/num, 0]);
+		uvs.push([(i+1)/num, 0]);
+		uvs.push([(i+1)/num, 1]);
+		uvs.push([i/num, 1]);
+
+		normals.push([0, -1, 0]);
+		normals.push([0, -1, 0]);
+		normals.push([0, -1, 0]);
+		normals.push([0, -1, 0]);
+
+		indices.push(count * 4 + 0);
+		indices.push(count * 4 + 1);
+		indices.push(count * 4 + 2);
+		indices.push(count * 4 + 0);
+		indices.push(count * 4 + 2);
+		indices.push(count * 4 + 3);
+		
+		extras.push([rotation, speed]);
+		extras.push([rotation, speed]);
+		extras.push([rotation, speed]);
+		extras.push([rotation, speed]);
+
+		count ++;
+
+
+		//	bottom
+		positions.push(getPos(i, y, height, radius, theta));
+		positions.push(getPos(i+1, y, height, radius, theta));
+		positions.push(getPos(i+1, y, height, radius-thickness, theta));
+		positions.push(getPos(i, y, height, radius-thickness, theta));
+
+		uvs.push([i/num, 0]);
+		uvs.push([(i+1)/num, 0]);
+		uvs.push([(i+1)/num, 1]);
+		uvs.push([i/num, 1]);
+
+		normals.push([0, 1, 0]);
+		normals.push([0, 1, 0]);
+		normals.push([0, 1, 0]);
+		normals.push([0, 1, 0]);
 
 		indices.push(count * 4 + 0);
 		indices.push(count * 4 + 1);
